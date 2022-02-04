412e73996e5f9ad398a98dd8783783110da41cb5
zhouyinn <yinnzho@gmail.com>
2022-02-04 14:41:14 -0600

    add CLI

--- src/org/neon/main/Main.java
+++ src/org/neon/main/Main.java
@@ -1,17 +1,43 @@
 package org.neon.main;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
-import org.neon.engine.XMLReader;
-import org.neon.model.Condition;
-import org.neon.model.Heuristic;
+import org.neon.engine.XMLWriter;
+import org.neon.model.Result;
 import org.neon.ui.OutputPanel;
 
 
 public class Main {
 	public static void main(String args[]) throws Exception{
-    	OutputPanel gui = new OutputPanel();
+		if (args.length == 0){
+			OutputPanel gui = new OutputPanel();
+		}else {
+			run(args[0], args[1], args[2]);
+		}
+	}
+
+	static void run(String inputTextPath, String inputXMLPath, String outputXMLPath) throws Exception {
+		// read input
+		BufferedReader br = new BufferedReader(new FileReader(inputTextPath));
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
+		while (line!=null){
+			sb.append(line+"\n");
+			line = br.readLine();
+
+		}
+		String textToClassify = sb.toString();
+
+		// analyze
+		File strategy = Path.of(inputXMLPath).toFile();
+		org.neon.engine.Parser parser = org.neon.engine.Parser.getInstance();
+		ArrayList<Result> results = parser.extract(textToClassify, strategy);
+		File destination = Path.of(outputXMLPath).toFile();
+		XMLWriter.addXMLSentences(destination, results);
 	}
 
 }	
